70e6a920dd751bdee195451204876576b8f45255
Gerbert Pesquera <gerbert.pesquera@gmail.com>
2022-05-09 16:18:31 -0400

    commit tree 44ad09c0a439ba109a5569eb3ce2a629044406ae

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 //const input = document.getElementById('input');
 
+/*
 function addingEventListener() {
     input.addEventListener('click', addingEventListener);
     alert('I was clicked!');
@@ -8,4 +9,19 @@ function addingEventListener() {
     
     
 }
+*///  Old Code 
 
+
+// New Method also permenant
+//const input = document.getElementById('input');
+function addingEventListener(){
+
+    //const input = document.getElementById('input');
+
+    function clickAlert() {
+    alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
+};
+addingEventListener()
